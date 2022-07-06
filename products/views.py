6cf5150501f2401a265c89c7cf283aa2706fec12
Malcolm MikaZuki <malcolm.wiredu@gmail.com>
2022-07-06 13:47:16 +0000

    added prodcuts, templates, models, views,urls

---
+++ products/views.py
@@ -0,0 +1,23 @@
+tance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
