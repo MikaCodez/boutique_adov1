f5ee56aa91e647143f5fabc299ce8666c3965853
Malcolm MikaZuki <malcolm.wiredu@gmail.com>
2022-07-13 19:07:50 +0000

    Email host settings updated

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -222,4 +222,15 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'pk_test_51LJFjoGWRsXFTFWwLYq2yqOTpQ52ZH3djhjtPW0Q2NgGM6qqARwlj1su0CdXCgG0FkWsWfopTAILVfgXM1TyEIlx00RnzLb3Hl')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'sk_test_51LJFjoGWRsXFTFWw6bpeuN2L1NwSRzxNEEUbj8cZSQdXA4VuoMUfKNNjleIDNljALHh4gwsorChCyNw0lCtpy4Ji00ThWzlvDh')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
